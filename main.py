9500eb91dcdf61302ff214bfa0de4bc3772c0f11
OmochaPL <94681221+OmochaPL@users.noreply.github.com>
2021-12-09 10:58:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+#moja wesja
+
+phrase = "Podaj jajko!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+for i in range(3):
+    plist.pop()
+plist.remove('P')
+plist.remove('a')
+plist.remove('j')
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
+
+
+# z książki
+
 phrase = "Podaj jajko!"
 plist = list(phrase)
 print(phrase)
